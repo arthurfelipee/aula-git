7f4c85ac036371578e675d5676c67b51127b4332
unknown <arthursantos.3743@aluno.saojudas.br>
2021-03-08 20:50:59 -0300

    complemento classe teste

--- Empregado.java
+++ Empregado.java
@@ -8,7 +8,29 @@ private double comissao;
 private double bonus;
 public double calcular salario (){
 
-    
+    if (tipo == 1){
+        return salaraio;
+    }
+    else if (tipo == 2){
+        return salario + salario * comissao;
+    }
+    else if (tipo == 3){
+        return salario + bonus;
+    }
+    else{
+        return 0;
+    }
 }
-
+    public String getNome (){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getIdade (){
+        return this.idade;
+    }
+    public void setIdade(String idade){
+        this.idade = idade;
+    }
 }
